e2fa56bad4f1a241c54b28f9dc929624723b5549
Or Zellinger <or.zellinger@gmail.com>
2025-06-25 13:09:22 +0300

    Added add magic method

--- Shape.py
+++ Shape.py
@@ -1,7 +1,13 @@
 class Shape:
 
+    def __init__(self):
+        pass
+
     def get_area(self):
         pass
 
     def __str__(self):
         return 'This Is The Base Class Of Shape'
+
+    def __add__(self, other):
+        return self.get_area() + other.get_area()
