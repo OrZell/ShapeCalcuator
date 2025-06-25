643ec42c92c043a63b960e584b20de355a25ac5e
Or Zellinger <or.zellinger@gmail.com>
2025-06-25 11:19:15 +0300

    Add perimeter and magic method add

--- Tringle.py
+++ Tringle.py
@@ -9,5 +9,11 @@ class Tringle(Rectangle):
     def get_area(self):
         return super().get_area()/2
 
+    def get_perimeter(self):
+        return super().get_perimeter()/2
+
     def __str__(self):
         return super().__str__()
+
+    def __add__(self, other):
+        return super().__add__()/2
