5745635eb318318b83d5b390d79d1f36ac8f7ac0
Or Zellinger <or.zellinger@gmail.com>
2025-06-25 11:16:02 +0300

    Add perimeter and magic method add

--- Square.py
+++ Square.py
@@ -9,5 +9,11 @@ class Square(Rectangle):
     def get_area(self):
         return super().get_area()
 
+    def get_perimeter(self):
+        return super().get_perimeter()
+
     def __str__(self):
         return f'Kind: {self.Name}, A: {self.A}'
+
+    def __add__(self, other):
+        return self.get_area() + other.get_area()
