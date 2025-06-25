2b609bc4421463f900659e1f6b9eafdc8847a1d7
Or Zellinger <or.zellinger@gmail.com>
2025-06-25 13:39:20 +0300

    added some magic methods

--- Circle.py
+++ Circle.py
@@ -19,3 +19,24 @@ class Circle(Shape):
 
     def __add__(self, other):
         return self.get_area() + other.get_area()
+
+    def __len__(self):
+        return self.get_perimeter()
+
+    def __lt__(self, other):
+        return super().__lt__(other)
+
+    def __le__(self, other):
+        return super().__le__(other)
+
+    def __eq__(self, other):
+        return super().__eq__(other)
+
+    def __ne__(self, other):
+        return super().__ne__(other)
+
+    def __ge__(self, other):
+        return super().__ge__(other)
+
+    def __gt__(self, other):
+        return super().__gt__(other)
