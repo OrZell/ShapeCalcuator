48c5fa7c787d03cf4e4e04c4c7f08f740e3ea280
Or Zellinger <or.zellinger@gmail.com>
2025-06-25 11:26:38 +0300

    Add perimeter and magic method add

--- Hexagon.py
+++ Hexagon.py
@@ -10,5 +10,11 @@ class Hexagon(Shape):
     def get_area(self):
         return pow(self.A, 2)*(3 * (pow(3, 0.5)) / 2)
 
+    def get_perimeter(self):
+        return self.A * 6
+
     def __str__(self):
         return f'Kind: {self.Name}, A: {self.A}'
+
+    def __add__(self, other):
+        return self.get_area() + other.get_area()
